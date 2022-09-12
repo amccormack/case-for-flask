d3127d729312301c83443b36438d8f9bdd500375
alex mccormack <amccormack>
2022-09-12 16:07:21 -0400

    adding example code

---
+++ hello.py
@@ -0,0 +1,32 @@
+import io
+from flask import Flask, request, send_file
+import make_chart
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            return 'No selected file'
+        input_bytes = file.read()
+        output_bytes = make_chart.excel_to_bytes(input_bytes)
+        output = io.BytesIO(output_bytes)
+        return send_file(output, mimetype='image/png')
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
---
+++ hello_upload.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            return 'No selected file'
+        return f'I saw the file {file.filename}'
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
